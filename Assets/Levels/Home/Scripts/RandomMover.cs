5b06aa45963b69462a8089d849745a1b66efb486
Nikita <starshov.n.a@gmail.com>
2017-09-04 12:28:18 +0300

    Some uptimization in struct of project

---
+++ Assets/Levels/Home/Scripts/RandomMover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMover : MonoBehaviour {
+
+
+	public float speed = 5.0f;
+
+	private Rigidbody2D rb2D;
+
+	// Use this for initialization
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();
+		Vector3 randomDirection = Random.insideUnitSphere;
+		randomDirection.z = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
